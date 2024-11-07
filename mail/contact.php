64764221261718ff20cff8a82a2c18bc1955ccda
architamalviya358 <architamalviya358@gmail.com>
2024-11-08 00:05:01 +0530

    mail issue

--- mail/contact.php
+++ mail/contact.php
@@ -15,6 +15,8 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
+
+}
 ?>
