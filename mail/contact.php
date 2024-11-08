7ae2265ad9ef969e18c94f1234a25dc9d5621776
architamalviya358 <architamalviya358@gmail.com>
2024-11-08 17:44:31 +0530

    fixing mail issue

--- mail/contact.php
+++ mail/contact.php
@@ -9,13 +9,21 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "ashutosh.namdev.9@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email" . "\r\n";
-$header .= "Reply-To: $email" . "\r\n";
+// $to = "ashutosh.namdev.9@gmail.com"; // Change this email to your //
+// $subject = "$m_subject:  $name";
+// $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+// $header = "From: $email" . "\r\n";
+// $header .= "Reply-To: $email" . "\r\n";
 
-if(!mail($to, $subject, $body, $header)){
+$to = "ashutosh.namdev.9@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+// mail($to,$subject,$txt,$headers);
+
+if(!mail($to,$subject,$txt,$headers)){
   http_response_code(500);
 }
 ?>
