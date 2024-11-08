4db0e68ca1126435d171a49c6cfbcbb9ec2e0cf5
architamalviya358 <architamalviya358@gmail.com>
2024-11-08 17:41:15 +0530

    fixing mail issue

--- mail/contact.php
+++ mail/contact.php
@@ -9,17 +9,13 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "mithsv20@gmail.com"; // Change this email to your //
+$to = "ashutosh.namdev.9@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email" . "\r\n";
-$header .= "Reply-To: $email" . "\r\n";	
+$header .= "Reply-To: $email" . "\r\n";
 
 if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
 }
-
-// if(!mail($to, $subject, $body)){
-//   http_response_code(500);
-// }
 ?>
